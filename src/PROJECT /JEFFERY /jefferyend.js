654e2b210d8d996c6d7348b7cba5fe57b2bd90c5
willowbullington <willowg@bu.edu>
2024-12-04 10:38:09 -0500

    final pushes

---
+++ src/PROJECT /JEFFERY /jefferyend.js
@@ -0,0 +1,20 @@
+// Select the correct-fish image
+const star1 = document.getElementById("star1");
+
+// Add styles to make the image look clickable
+
+
+star1.style.cursor = "pointer";
+
+// Add an event listener to navigate to correct.html when the image is clicked
+star1.addEventListener("click", () => {
+    window.location.href = "page3.html";
+});
+
+const star2=document.getElementById (star2)
+
+ star2.style.cursor= "pointer";
+
+ star2.addEventListeneraddEventListener("click", () => {
+	window.location.href = "index.html";
+});
