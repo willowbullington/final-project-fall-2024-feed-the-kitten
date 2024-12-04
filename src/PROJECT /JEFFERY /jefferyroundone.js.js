0307c39edfda59a7eb2dc52f5c4efca618b51cdd
willowbullington <willowg@bu.edu>
2024-12-04 10:13:02 -0500

    updates

--- src/PROJECT /JEFFERY /jefferyroundone.js.js
+++ src/PROJECT /JEFFERY /jefferyroundone.js.js
@@ -1,7 +1,8 @@
 // Select the correct-fish image
-const correctfish = document.getElementById("correctfish");
+const correctfish = document.getElementById("correct-fish");
 
 // Add styles to make the image look clickable
+
 correctfish.style.cursor = "pointer";
 
 // Add an event listener to navigate to correct.html when the image is clicked
