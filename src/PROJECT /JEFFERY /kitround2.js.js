335fbc88cb2e612d4e2dbb2e598cec90996a154e
willowbullington <willowg@bu.edu>
2024-12-04 12:07:14 -0500

    final pushes

--- src/PROJECT /JEFFERY /kitround2.js.js
+++ src/PROJECT /JEFFERY /kitround2.js.js
@@ -1,16 +1,21 @@
+// Select the correct-fish image
+const bluefish = document.getElementById("bluefish");
+
+// Add styles to make the image look clickable
+bluefish.style.cursor = "pointer";
 
 // Add an event listener to navigate to correct.html when the image is clicked
 bluefish.addEventListener("click", () => {
-	window.location.href = "page101.html";
+    window.location.href = "page71.html";
 });
 
-// Select the correct-fish image
+// Select the incorrect-fish image
 const incorrect = document.getElementById("incorrect");
 
 // Add styles to make the image look clickable
-incorrect.cursor = "pointer";
+incorrect.style.cursor = "pointer"; // Fixed this line
 
 // Add an event listener to navigate to correct.html when the image is clicked
 incorrect.addEventListener("click", () => {
-	window.location.href = "page91.html";
+    window.location.href = "page81.html";
 });
